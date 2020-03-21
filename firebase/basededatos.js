70b379b1929aedd041ffe40e3428f830762e868a
Byron Meza <byronrm99@outlook.com>
2020-03-20 20:15:52 -0600

    Actualizacion

---
+++ firebase/basededatos.js
@@ -0,0 +1,12 @@
+    var firebaseConfig = {
+        apiKey: "AIzaSyDEEubn0lBDtyM7OKzV_RDxT6oiNgGWOvo",
+        authDomain: "proyectobyron67344.firebaseapp.com",
+        databaseURL: "https://proyectobyron67344.firebaseio.com",
+        projectId: "proyectobyron67344",
+        storageBucket: "proyectobyron67344.appspot.com",
+        messagingSenderId: "530981578067",
+        appId: "1:530981578067:web:0464b4768b65d217b5ada6"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
